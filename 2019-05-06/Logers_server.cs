c87b2f31d7fa72112e2fbbe383920b4f132f3388
Mikolonija <bieliauskasmodestas47@gmail.com>
2019-05-06 22:22:44 +0300

    add files

---
+++ 2019-05-06/Logers_server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+
+namespace _2019_05_06
+{
+    public class Logers_server
+    {
+        string filePath = string.Format("logai {0}.log", DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss"));
+        public void Init()
+        {
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Dispose();
+            }
+
+        }
+        public void WriteLog(string log)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true)) {
+                writer.WriteLine(log);
+                Console.WriteLine(log);
+
+            };
+        }
+
+    
+
+
+    }
+}
---
+++ 2019-05-06/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+
+namespace _2019_05_06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Logers_server LogService = new Logers_server();
+            LogService.Init();
+
+            String driverPath = Environment.CurrentDirectory;
+            ChromeOptions chromeOptions = new ChromeOptions();
+
+            
+
+            try
+            {
+                using (ChromeDriver browserDriver = new ChromeDriver(driverPath, chromeOptions))
+                {
+                    LogService.WriteLog("Mode");
+                    browserDriver.Manage().Window.Maximize();
+                    Actions actions = new Actions(browserDriver);
+                    browserDriver.Url = "https://www.filmai.in/";
+                    browserDriver.FindElementByClassName("loginBtn").Click();
+                    browserDriver.FindElementByName("login_name").SendKeys("Mode");
+                    browserDriver.FindElementByName("login_password").SendKeys("Mode");
+                    browserDriver.FindElementByClassName("btn btn-primary btn-block").Click();
+                    LogService.WriteLog("succses");
+
+                }
+
+            }
+ 
+
+            catch (Exception e)
+            {
+
+                LogService.WriteLog(e.ToString());
+            }
+        }
+    }
+}
